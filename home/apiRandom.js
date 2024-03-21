e602bbc9420e4b30b7b7a0ef9bc7f98ad2dca63c
korapat699-7 <150796921+korapat699-7@users.noreply.github.com>
2024-03-21 21:21:52 +0700

    ียกฟะำ

--- home/apiRandom.js
+++ home/apiRandom.js
@@ -5,6 +5,8 @@ fetch("https://openlibrary.org/subjects/comics_&_graphic_novels_manga_science_fi
   .then((data) => {
     console.log(data);
     for(let j = 0; j < 12; j ++ ) {
+        const img1 = document.querySelector(".img-random" + (j + 1));
+        img1.src = "https://covers.openlibrary.org/b/id/" + data.works[j].cover_id + ".jpg";
         const title_random = document.querySelector(".name-random" + (j + 1));
         title_random.innerHTML = data.works[j].title ;
     
@@ -28,6 +30,9 @@ fetch("https://openlibrary.org/subjects/comics_&_graphic_novels_manga_science_fi
 
     console.log(data);
     for(let j = 0; j < 13; j ++ ) {
+        const img1 = document.querySelector(".img-random" + (j + 13));
+        img1.src = "https://covers.openlibrary.org/b/id/" + data.works[j].cover_id + ".jpg";
+        img1.alt = data.works[j].title ;;
         const title_random = document.querySelector(".name-random" + (j + 13));
         title_random.innerHTML = data.works[j].title ;
     
--- home/apiTrending.js
+++ home/apiTrending.js
@@ -5,7 +5,6 @@ fetch("https://openlibrary.org/trending/daily.json")
   })
   .then((data) => {
     for (let i = 0; i < 10; i++) {
-      console.log(data.works[i].title + " " + data.works[i].author_name + data.works[i].cover_i);
       const img = document.querySelector(".img-rank" + (i + 1));
       img.src = "https://covers.openlibrary.org/b/id/" + data.works[i].cover_i + ".jpg";
       
